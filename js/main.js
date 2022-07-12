56f5bb240ff0ba7c770632c0cb55e4455c40a6e9
NaimurRahman <naimur.rahman900@gmail.com>
2022-07-13 00:39:21 +0600

    dfd

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function toNetflix(){
+    document.getElementById("netflix")
+    window.location.href = "netflix.html";
+}
+
+const boxes = document.querySelectorAll(".box");
+const checkBoxes = () => {
+    const triggerBotttom = (window.innerHeight / 5) * 4;
+    boxes.forEach((box) =>{
+        const boxTop = box.getBoundingClientRect().top;
+        if(boxTop< triggerBotttom) box.classList.add("show");
+        else box.classList.remove("show");
+    });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes();
