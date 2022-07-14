f7a2f7b05cf0ef84b04564f4ea016edf3599c443
NaimurRahman <naimur.rahman900@gmail.com>
2022-07-15 00:52:37 +0600

    never ending

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,12 @@
-const getUrl = window.location.href;
-const price = getUrl.substr(56);
-console.log(price)
-
+let urlString = window.location.href;
+len = urlString.length;
+price = ""
+for(let i=len-1; i>=0 ; i--){
+    if(urlString[i] == "="){
+        break;
+    }
+    price = urlString[i]+price;
+}
 document.querySelector("#subscription_price").innerHTML = price + "tk";
+
+
