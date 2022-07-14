df5b806d5a9d450543c4185792db17b13f22a244
NaimurRahman <naimur.rahman900@gmail.com>
2022-07-14 17:29:01 +0600

    fixxxx

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,5 @@
 const getUrl = window.location.href;
-const price = getUrl.substr(38);
+const price = getUrl.substr(56);
 console.log(price)
 
 document.querySelector("#subscription_price").innerHTML = price + "tk";
